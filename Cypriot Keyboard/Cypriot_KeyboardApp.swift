addbc3a3f0b452508d0bd882e1bc1726a5dada79
Alex Toumazis <alex.toumazis@gmail.com>
2021-02-04 16:52:03 -0800

    Initial Commit

---
+++ Cypriot Keyboard/Cypriot_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cypriot_KeyboardApp.swift
+//  Cypriot Keyboard
+//
+//  Created by Alex Toumazis on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Cypriot_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
