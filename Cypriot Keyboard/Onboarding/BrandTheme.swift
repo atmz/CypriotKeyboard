3e467e9a1888e00a49fe1a9be4b3429402ddf87c
Alex Toumazis <alext@mac.lan>
2026-05-06 22:13:48 +0300

    onboarding: brand color tokens (copper #D57800, olive #6B7F3A)

---
+++ Cypriot Keyboard/Onboarding/BrandTheme.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+extension Color {
+    static let cypriotCopper = Color(red: 0xD5 / 255.0, green: 0x78 / 255.0, blue: 0x00 / 255.0)
+    static let cypriotOlive  = Color(red: 0x6B / 255.0, green: 0x7F / 255.0, blue: 0x3A / 255.0)
+}
